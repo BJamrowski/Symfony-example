27cef3244f82994d131b8f897bb5a0fec7c6aa8d
seba-gra <sebastiangrabinski@tlen.pl>
2021-07-26 10:09:30 +0200

    Add 403 error handler

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler extends AbstractController implements AccessDeniedHandlerInterface
+{
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?RedirectResponse
+    {
+        $this->addFlash(
+            'notice',
+            'Access denied'
+        );
+        return ($this->redirectToRoute('home'));
+    }
+}
